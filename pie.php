b25ff348bdfe11a062b39b103422f9961cdbc644
claudiamarisol <128396290+claudiamarisol@users.noreply.github.com>
2023-05-03 21:38:17 -0400

    SE SUBIO PRACTICA4

---
+++ pie.php
@@ -0,0 +1,8 @@
+<!--pie.php-->
+<footer class="px-2 py-2 fixed-bottom bg-dark text-center">
+<span class="text-white">By CLAUDIA MARISOL CACHI Q. <?php echo date("Y"); ?>
+</span>
+</footer>
+</main>
+</body>
+</html>
